ab707cc54e45fac62ab244929d2506c39b26b8f0
Rolf Koenders <post@madebyrolf.nl>
2014-08-28 09:33:10 +0200

    Making sound with the WebkitAudio API

---
+++ #177 [Intermediate] Morse/Morse.js
@@ -0,0 +1,75 @@
+var MorseTable = {
+	A: '.-', B: '-...', C: '-.-.', D: '-..',
+    E: '.', F: '..-.', G: '--.', H: '....',
+    I: '..', J: '.---', K: '-.-', L: '.-..',
+    M: '--', N: '-.', O: '---', P: '.--.',
+    Q: '--.-', R: '.-.', S: '...', T: '-',
+	U: '..-', V: '...-', W: '.--', X: '-..-',
+	Y: '-.--', Z: '--..', " ": "/"
+};
+
+var durations = {
+    ".": 50,
+    "-": 300,
+    " ": 200,
+    "/": 300
+};
+
+var context = new webkitAudioContext();
+
+window.onload = function() {
+    document.getElementById("translate").addEventListener("click", translateMorse);
+    document.getElementById("listen").addEventListener("click", playMorse);
+};
+
+function translateMorse() {
+    var inputText = document.getElementById("input").value.toLowerCase();
+    var morseText = "";
+
+    for (var i = 0; i < inputText.length; i++) {
+        morseText += MorseTable[inputText[i].toUpperCase()] + " ";
+    }
+
+    document.getElementById("outputDiv").style.display = "block";
+    document.getElementById("morse").innerHTML = morseText;
+}
+
+function playMorse() {
+    var morseText = document.getElementById("morse").innerHTML;
+    var playSequence = [];
+
+    for (var i = 0; i < morseText.length; i++) {
+        playSequence.push({
+            duration: durations[morseText[i]],
+            tone: (morseText[i] === '.' || morseText[i] === '-')
+        });
+    }
+
+    playTone(playSequence, 0);
+}
+
+function playTone(sequence, index) {
+    if (index < sequence.length) {
+        var duration = sequence[index].duration;
+
+        if (sequence[index].tone) {
+            var oscillator = context.createOscillator();
+            oscillator.type = 0;
+            oscillator.frequency.value = 800;
+            oscillator.connect(context.destination);
+            oscillator.noteOn && oscillator.noteOn(0);
+
+            setTimeout(function() {
+                oscillator.noteOff(0);
+                oscillator.disconnect();
+                setTimeout(function() {
+                    playTone(sequence, index + 1);
+                }, 100);
+            }, duration);
+        } else {
+            setTimeout(function() {
+                playTone(sequence, index + 1);
+            }, duration);
+        }
+    }
+}
